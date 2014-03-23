c37a560f70e5ddc63981927c202c3fc1af5a6800
frankolson <will.olson11@gmail.com>
2014-03-23 10:47:06 -0700

    Finished the arduinoPiano program and strated the extra credit shutterSpeed program for the hw. looking gooooddd.

--- lab6/shutterSpeed/shutterSpeed.ino
+++ lab6/shutterSpeed/shutterSpeed.ino
@@ -1,17 +1,17 @@
 /*
-Arduino Piano
+Arduino Camera Shutter
 Frank Olson
 V.1
-10 March 2014
+22 March 2014
 Arduino Mega 2560 R3
-Last Update: 11 March 2014
+Last Update: 23 March 2014
 
 Simple Serial ECHO script : Written by ScottC 03/07/2012
 
-This program is for lab 6. Using the new delay function created and the simple
-serial connection code cited above create a program that takes computer input
-and outputs tones like a piano on portB.6 on the Arduino Mega with a 50% duty
-cycle.
+This program is for the extra credit on homework. Using the new delay function
+created, develop a program that controls a camera shutter on portB.7. Shutter
+speeds will be passed through computer using the serial monitor and the echo
+script cited above. 0 should be off and 1 should be on.
 */
 
 // Define hardware pointers
@@ -23,29 +23,28 @@ volatile unsigned char *myTIMSK1;   // Timer/Counter 1 Interupt Mask Register
 volatile unsigned char *myTIFR1;    // Timer/Counter 1 Interupt Flag Register
 volatile unsigned int  *myTCNT1;    // Timer/Counter 1 16-bit Counter Register
 
-// Define Frequency Tones (lowercase stands fo sharpnote):
-//                     Note       Tone         Frequency
-volatile unsigned long A     =    2272;     // 440 Hz
-volatile unsigned long a     =    2146;     // 466 Hz
-volatile unsigned long B     =    2024;     // 494 Hz
-volatile unsigned long C     =    1912;     // 523 Hz
-volatile unsigned long c     =    1805;     // 554 Hz
-volatile unsigned long D     =    1704;     // 587 Hz
-volatile unsigned long d     =    1603;     // 624 Hz
-volatile unsigned long E     =    1517;     // 659 Hz
-volatile unsigned long F     =    1433;     // 698 Hz
-volatile unsigned long f     =    1351;     // 740 Hz
-volatile unsigned long G     =    1276;     // 784 Hz
-volatile unsigned long g     =    1203;     // 831 Hz
+// Define shutter speeds:
+//                     Input      Speed (us)      Shutter Speed (s)
+volatile unsigned long 0     =    1000000;     // 1
+volatile unsigned long 1     =    500000;      // 1/2
+volatile unsigned long 2     =    250000;      // 1/4
+volatile unsigned long 3     =    125000;      // 1/8
+volatile unsigned long 4     =    66666;       // 1/15
+volatile unsigned long 5     =    33333;       // 1/30
+volatile unsigned long 6     =    16666;       // 1/60
+volatile unsigned long 7     =    8000;        // 1/125
+volatile unsigned long 8     =    4000;        // 1/250
+volatile unsigned long 9     =    2000;        // 1/500
+volatile unsigned long 10    =    1000;        // 1/1000
+
 
 // Define functions
 void newDelay(unsigned long mSeconds);
 long convertInput(byte value);
 
 // Use a variable called byteRead to store the data coming
-// from the computer. Use as tone and defualt to 440Hz
-// Note: 0b01000001 is 'A' in binary
-byte byteRead = 0b01000001;
+// from the computer.
+byte byteRead;
 
 void setup() {                
   // Turn the Serial Protocol ON
@@ -70,9 +69,10 @@ void setup() {
   portDDRB = (unsigned char *) 0x24;
   *portDDRB |= 0x80;
   
-  Serial.println("Notes are A, A#, B, C, C#, D, D#, E, F, F#, G, G#.");
-  Serial.println("Arduino will default at A. Write sharp notes lower case.");
-  Serial.println("Please Enter the note you would like to play: ");
+  // Message to user/prompt
+  Serial.println("Shutter Speeds are 1, 1/2, 1/4, 1/8, 1/15, 1/30, 1/60, ");
+  Serial.println("1/125, 1/250, 1/500, 1/1000. Enter 0 through 10  to");
+  Serial.println("represent the speed, 0 being 1, 1 being 1/2 and so forth.");
 }
 
 void loop() {
@@ -84,20 +84,20 @@ void loop() {
     Serial.write(byteRead);
     Serial.println();
     // Message to user/prompt
-    Serial.println("Notes are A, A#, B, C, C#, D, D#, E, F, F#, G, G#.");
-    Serial.println("Arduino will default at A. Write sharp notes lower case");
-    Serial.println("Please Enter the note you would like to play: ");
+    Serial.println("Shutter Speeds are 1, 1/2, 1/4, 1/8, 1/15, 1/30, 1/60, ");
+    Serial.println("1/125, 1/250, 1/500, 1/1000. Enter 0 through 10  to");
+    Serial.println("represent the speed, 0 being 1, 1 being 1/2 and so forth.");
   }
   
   portB = (unsigned char *) 0x25; 
   
-  // Flip portB then use byteRead/2 to define pulses
+  // Flip portB then use byteRead to define shutter speed
   *portB ^= 0x80;
-  newDelay(convertInput(byteRead)/2);
+  newDelay(convertInput(byteRead));
   
 }
 
-// Delay Function
+// Delay Function (microseconds)
 void newDelay(unsigned long mSeconds) {
   // used a prescaler of 64 with mSeconds/4 scaling converting to microseconds
   *myTCNT1 = (unsigned int) (65536 - (long) (mSeconds/4));
@@ -113,41 +113,38 @@ void newDelay(unsigned long mSeconds) {
 
 // Convert input to tone
 long convertInput(byte value) {
-  if(value == 'A'){
-    return A;
+  if(value == '0'){
+    return 0;
   }
-  else if(value == 'a'){
-    return a;
+  else if(value == '1'){
+    return 1;
   }
-  else if(value == 'B'){
-    return B;
+  else if(value == '2'){
+    return 2;
   }
-  else if(value == 'C'){
-    return C;
+  else if(value == '3'){
+    return 3;
   }
-  else if(value == 'c'){
-    return c;
+  else if(value == '4'){
+    return 4;
   }
-  else if(value == 'D'){
-    return D;
+  else if(value == '5'){
+    return 5;
   }
-  else if(value == 'd'){
-    return d;
+  else if(value == '6'){
+    return 6;
   }
-  else if(value == 'E'){
-    return E;
+  else if(value == '7'){
+    return 7;
   }
-  else if(value == 'F'){
-    return F;
+  else if(value == '8'){
+    return 8;
   }
-  else if(value == 'f'){
-    return f;
+  else if(value == '9'){
+    return 9;
   }
-  else if(value == 'G'){
-    return G;
-  }
-  else if(value == 'g'){
-    return g;
+  else if(value == '10'){
+    return 10;
   }
 }
 
