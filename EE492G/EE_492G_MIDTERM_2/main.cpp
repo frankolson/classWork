5949a4a2412f21a2ce2148e3dae0c4b099223267
Will Olson <will.olson11@gmail.com>
2015-04-03 00:37:22 -0700

    Large cell segmentation is completed, I now need to to work on the light spots within each cell

--- EE492G/EE_492G_MIDTERM_2/main.cpp
+++ EE492G/EE_492G_MIDTERM_2/main.cpp
@@ -6,100 +6,102 @@
 using namespace cv;
 using namespace std;
 
-/////////////////////////////////////// Segmentation /////////////////////////////////////////////
-Mat segmentation(Mat image){
+//////////////////////////////////////// Get Ellipse /////////////////////////////////////////////
+void findEllipse(Mat& img, Mat& final){
+	// find contours
+	vector<vector<Point> > contours;
+	findContours(img, contours, RETR_LIST, CHAIN_APPROX_NONE);
+
+	// fit the ellipses
+	for (size_t i = 0; i < contours.size(); i++){
+		size_t count = contours[i].size();
+        if( count < 6 )
+            continue;
+
+        Mat pointsf;
+        Mat(contours[i]).convertTo(pointsf, CV_32F);
+        RotatedRect box = fitEllipse(pointsf);
+
+        if( MAX(box.size.width, box.size.height) > MIN(box.size.width, box.size.height)*30 )
+            continue;
+        drawContours(final, contours, (int)i, Scalar::all(255), 1, 8);
+
+        ellipse(final, box, Scalar(0,0,255), 1, LINE_AA);
+        ellipse(final, box.center, box.size*0.5f, box.angle, 0, 360, Scalar(0,255,255), 1, LINE_AA);
+        Point2f vtx[4];
+        box.points(vtx);
+        //for( int j = 0; j < 4; j++ )
+            //line(cimage, vtx[j], vtx[(j+1)%4], Scalar(0,255,0), 1, LINE_AA);
+	}
+
+	// show results in a sizable window
+	namedWindow("Result", WINDOW_NORMAL);
+	imshow("Result", final);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////// Remove Noise /////////////////////////////////////////////
+void removeSmallBlobs(Mat& im, int thresh)
+{
+    // Only accept CV_8UC1
+    if (im.channels() != 1 || im.type() != CV_8U){
+        cout << "not the right type" << endl;
+        return;
+    }
+
+    // Find all contours
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(im.clone(), contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+
+    for (size_t i = 0; i < contours.size(); i++){
+        // look for hierarchy[i][3]!=-1, ie hole boundaries
+        if ( (hierarchy[i][3] != -1) && (contourArea(contours[i]) < thresh) ) {
+            // fill with white
+            drawContours( im, contours, i, Scalar(255,255,255),-1  );
+        }
+        //drawContours(im, contours, i, Scalar(255,255,255),-1);
+    }
+    cout << endl << endl;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////// Segmentation Large //////////////////////////////////////////
+void segmentationLarge(Mat img){
 	// grayscale
 	Mat binary;
 	// Split Channels
 	Mat channel[3];
-	split(image,channel);
+	split(img,channel);
 	channel[0].convertTo(binary, CV_8U);
-	imshow("BLUE", binary);
 
-	// Reduce Noise
-	GaussianBlur(binary, binary, Size(5, 5), 0.5, 0.5); //applying Gaussian filter
-	GaussianBlur(image, image, Size(5, 5), 0.5, 0.5); //applying Gaussian filter
+	//OTSU and remove noise
+	threshold(binary, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+	removeSmallBlobs(binary, 70);
 
-	//OTSU and remove noise200,
-	threshold(binary, binary, 0, 255, THRESH_BINARY | THRESH_OTSU); //TO-DO: bwAreaOpen(matlab)
+	// Find Large Cells
+	findEllipse(binary, img);
 
-	// Calculate distance transform
-	Mat objects, fg;
-	Mat dist = Mat::zeros(binary.size(), CV_8U);
-	distanceTransform(binary, dist, DIST_L2, 3);
-	normalize(dist, dist, 0, 1.0, NORM_MINMAX);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// Threshold the distance transform results
-	vector<vector<Point> > contours;
-	threshold(dist, objects, 0, 255, THRESH_BINARY);
+//////////////////////////////////// Segmentation Small //////////////////////////////////////////
+void segmentationSmall(Mat img){
+	// grayscale
+	Mat binary;
+	// Split Channels
+	Mat channel[3];
+	split(img,channel);
+	channel[1].convertTo(binary, CV_8U);
 
-	// Dilate threshold results
-	dilate(dist, fg, Mat(), Point(-1, -1), 10);
-	dilate(objects, objects, Mat(), Point(-1, -1), 10);
+	//OTSU and remove noise
+	threshold(binary, binary, 128, 255, THRESH_BINARY );
+	imshow("Result", binary);
+	removeSmallBlobs(binary, 1);
 
-	// Subtract small foreground from large foreground
-	fg = fg - dist;
+	// Find Large Cells
+	//findEllipse(binary, img);
 
-	// Threshold and convert results
-	threshold(fg, fg, 0, 255, THRESH_BINARY);
-	fg.convertTo(fg, CV_8U);
-	objects.convertTo(objects, CV_8U);
-
-	// cluster peaks
-	bitwise_xor(fg, objects, fg);
-	dilate(fg, fg, cv::Mat(), cv::Point(-1, -1), 3);
-
-	// Identify image pixels without objects
-	Mat bg;
-	dilate(binary, bg, Mat(), Point(-1, -1), 1);
-	threshold(bg, bg, 1, 128, cv::THRESH_BINARY_INV);
-
-	// connected components used to separate each peak (bean)
-	// This is really important as  the watershed algorithm needs
-	// separately identified intensity value valleys in order to
-	// segment properly
-	Mat dt;
-	fg.copyTo(dt);
-	dt.convertTo(dt, CV_8U);
-	Mat lbl(dt.size(), CV_32S);
-	int ncc = connectedComponents(dt, lbl, 4);
-	cout << "Number of Beans: " << ncc - 1 << endl;
-
-	// Create a gray color spectrum to paint the peaks
-	vector<unsigned char> colors(ncc);
-	colors[0] = 0; // for background
-	for (int label = 1; label < ncc; ++label)
-		colors[label] = 255 / ncc*label;
-
-	// Paint every separate peak with a shade of gray
-	for (int x = 0; x < dt.rows; x++) {
-		for (int y = 0; y < dt.cols; y++) {
-			int label = lbl.at<int>(x, y);
-			dt.at<unsigned char>(x, y) = colors[label];
-		}
-	}
-
-	// Create markers image
-	Mat markers(binary.size(), CV_8U, Scalar(0));
-	markers = dt + bg;
-
-	// Convert to image of ints
-	markers.convertTo(markers, CV_32S);
-
-	// Apply watershed
-	watershed(image, markers);
-
-	// show segmentation
-	Mat tmp;
-	markers.convertTo(tmp, CV_8U);
-
-	// show watershed results
-	Mat tmp2;
-	markers.convertTo(tmp2, CV_8U, 255, 255);
-	namedWindow( "watershed", WINDOW_NORMAL );
-	imshow("watershed", tmp2);
-
-	return tmp2;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -120,12 +122,13 @@ int main(int argc, char** argv){
 		return -1;
 	}
 
-	namedWindow("Original", WINDOW_AUTOSIZE);
+	// Show original image
+	namedWindow("Original", WINDOW_NORMAL);
 	imshow("Original", image);
 
 	// Segment
-	Mat segment;
-	segment = segmentation(image);
+	segmentationLarge(image.clone());
+	segmentationSmall(image.clone());
 
 	waitKey(0);
 	return 0;
